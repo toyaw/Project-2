47a882c30c3730df02fffd858295e198d3a0a259
Toya Williams <toyaw7@gmail.com>
2019-07-01 18:53:58 -0400

    added more to schema

--- models/service.js
+++ models/service.js
@@ -20,10 +20,22 @@ const ServiceSchema = new mongoose.Schema({
  */
 const ServiceCollection = mongoose.model('Service', ServiceSchema)
 
-
+/* Step 4
+ *
+ * TODO: delete this it's just a sample
+ *
+ */
+function getServiceByQuoteId(shopId) {
+    return ServiceCollection.find({quoteId: quoteId})
+  }
+  
+  function addService(serviceObject) {
+    return ServiceCollection.create(serviceObject)
+  }
+  
 //Export the model
 module.exports = {
-    
+
     getServiceByQuoteId,
     addService
   }
